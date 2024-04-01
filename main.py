692d1ddf8cba98c41799479c9466360da29a6ed1
Fabien Rica <fabien.rica@gmail.com>
2024-04-01 17:25:22 +0200

    fix #2 + extra cleanup

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+from logging.config import dictConfig
 import pycountry
 import reverse_geocoder as rg
 from exif import Image
 from flask import Flask, render_template, request, redirect, flash
-
 import utils
+
+# Settings to get similar output as Flask werkzeug logs
+# See https://flask.palletsprojects.com/en/3.0.x/logging/
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -37,6 +55,7 @@ def locate():
 
         if not utils.check_if_exif(image):
             flash('No Exif data found', 'error')
+            app.logger.warning("No Exif data found")
             return redirect("/")
 
         if request.form["action"] == "locate":
@@ -47,19 +66,18 @@ def locate():
             gps_longitude_ref = image.get("gps_longitude_ref")
             if not (gps_latitude or gps_latitude_ref or gps_longitude or gps_longitude_ref):
                 flash('No geolocation data found', 'error')
+                app.logger.warning("No geolocation data found")
                 return redirect("/")
 
             decimal_latitude = utils.dms_coordinates_to_dd_coordinates(gps_latitude, gps_latitude_ref)
-            print(f"Latitude (DD): {decimal_latitude}")
+            app.logger.debug(f"Latitude (DD): {decimal_latitude}")
             decimal_longitude = utils.dms_coordinates_to_dd_coordinates(gps_longitude, gps_longitude_ref)
-            print(f"Longitude (DD): {decimal_longitude}")
+            app.logger.debug(f"Longitude (DD): {decimal_longitude}")
             coordinates = (decimal_latitude, decimal_longitude)
-            location_info = rg.search(coordinates)[0]
+            location_info = rg.search(coordinates, verbose=False)[0]
             location_info['country'] = pycountry.countries.get(alpha_2=location_info['cc'])
-            print(f"{location_info}\n")
-            print(f"You were in {location_info['name']}, {location_info['country'].name}!")
             answer = f"{location_info['name']}, {location_info['country'].name}"
-            app.logger.info(f"Location: {answer}")
+            app.logger.info(f"You were in {answer}!")
             return render_template("locate.html", answer=answer,
                                    lat=decimal_latitude, lon=decimal_longitude)
         elif request.form["action"] == "details":
@@ -71,7 +89,7 @@ def locate():
                                 (not tags.startswith("get")) and
                                 (not tags.startswith("delete")) and
                                 (not tags.startswith("list_all")))
-            print(image_members)
+            app.logger.debug(image_members)
 
             # just to get a cleaner output if not capitalized
             model = image.model.capitalize()
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
+import logging
 import webbrowser
+
 
 # See https://auth0.com/blog/read-edit-exif-metadata-in-photos-with-python/
 
@@ -18,42 +20,26 @@ def dms_coordinates_to_dd_coordinates(coordinates, coordinates_ref):
     return decimal_degrees
 
 
+# Not used anymore, I keep it for cli app
 def draw_map_for_location(latitude, latitude_ref, longitude, longitude_ref):
-
     url = f"https://www.google.com/maps?q={latitude}{latitude_ref} {longitude}{longitude_ref}"
     # print(f"url is {url}")
     webbrowser.open_new_tab(url)
 
 
-def draw_map_for_city(city):
-
-    url = f"https://www.google.com/maps?q={city}"
-    # print(f"url is {url}")
-    webbrowser.open_new_tab(url)
-
-
 def check_if_exif(image):
+    """ Check if image has exif tags """
     ret = True
     if image.has_exif:
         status = f"contains EXIF (version {image.get('exif_version')}) information."
     else:
         status = "does not contain any EXIF information."
         ret = False
-    print(f"Image : {status}")
+    logging.debug(f"Image : {status}")
     return ret
 
 
-# def check_if_exif():
-#     ret = True
-#     if image.has_exif:
-#         status = f"contains EXIF (version {image.exif_version}) information."
-#     else:
-#         status = "does not contain any EXIF information."
-#         ret = False
-#     print(f"Image : {status}")
-#     return ret
-
-
+# Not used anymore, I keep it for cli app
 def display_details(image):
     image_members = [dir(image)]
     image_member_list = enumerate(image_members)
@@ -68,43 +54,3 @@ def display_details(image):
     print("-------------------------")
     print(f"{image.datetime_original}.{image.subsec_time_original} {image.get('offset_time', '')}\n")
     return image_member_list
-
-
-# if __name__ == '__main__':
-#
-#     photo_file = open("images/gent.jpg", "rb")
-#     image = Image(photo_file)
-#
-#     if not check_if_exif():
-#         exit()
-#
-#     display_details(image)
-#
-#     print(f"Coordinates - Image ")
-#     print("---------------------")
-#     # Latitude (Degrees, Minutes (1/60th of a degree)
-#     #     Seconds (1/60th of a minute, or 1/3600th of a degree))
-#     # _ref is the direction
-#     print(f"Latitude (DMS): {format_dms_coordinates(image.gps_latitude)}{image.gps_latitude_ref}")
-#     print(f"Longitude (DMS): {format_dms_coordinates(image.gps_longitude)}{image.gps_longitude_ref}\n")
-#
-#     # google maps works with decimal degrees
-#     decimal_latitude = dms_coordinates_to_dd_coordinates(image.gps_latitude, image.gps_latitude_ref)
-#     print(f"Latitude (DD): {decimal_latitude}")
-#     decimal_longitude = dms_coordinates_to_dd_coordinates(image.gps_longitude, image.gps_longitude_ref)
-#     print(f"Longitude (DD): {decimal_longitude}")
-#     print(f"Location info")
-#     print("-----------------------")
-#     coordinates = (decimal_latitude, decimal_longitude)
-#     location_info = rg.search(coordinates)[0]
-#     location_info['country'] = pycountry.countries.get(alpha_2=location_info['cc'])
-#     print(f"{location_info}\n")
-#     print(f"Final answer")
-#     print("-----------------------")
-#     print(f"You were in {location_info['name']}, {location_info['country'].name}!")
-#
-#     draw_map_for_location(decimal_latitude, image.gps_latitude_ref,
-#                           decimal_longitude, image.gps_longitude_ref)
-#
-#
-#
