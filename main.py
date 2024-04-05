54667c74e4d735f12d7c5f51efd30bd2625441fa
Fabien Rica <fabien.rica@gmail.com>
2024-04-05 21:14:50 +0200

    fix for routes issue: use of formaction, new details route (see #5), thx nice redditor person!

--- main.py
+++ main.py
@@ -27,6 +27,8 @@ dictConfig({
 })
 
 app = Flask(__name__)
+
+# TODO save the secret key somewhere else 
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
@@ -37,6 +39,7 @@ def home():
 
 @app.route("/version", methods=["GET"], strict_slashes=False)
 def version():
+    """ returns the server version stored in config.py for easier debugging """
     response_body = {
         "success": 1,
     }
@@ -64,42 +67,46 @@ def locate():
     image = Image(file.stream.read())
 
     if not utils.check_if_exif(image):
-        flash('No Exif data found', 'error')
-        app.logger.warning("No Exif data found")
+        flash('No EXIF data found', 'error')
+        app.logger.warning("No EXIF data found")
         return redirect("/")
 
-    if request.form["action"] == "locate":
-        # google maps works with decimal degrees, not dms
-        gps_latitude = image.get("gps_latitude")
-        gps_latitude_ref = image.get("gps_latitude_ref")
-        gps_longitude = image.get("gps_longitude")
-        gps_longitude_ref = image.get("gps_longitude_ref")
+    gps_latitude = image.get("gps_latitude")
+    gps_latitude_ref = image.get("gps_latitude_ref")
+    gps_longitude = image.get("gps_longitude")
+    gps_longitude_ref = image.get("gps_longitude_ref")
 
-        if not (gps_latitude or gps_latitude_ref or gps_longitude or gps_longitude_ref):
-            flash('No geolocation data found', 'error')
-            app.logger.warning("No geolocation data found")
-            return redirect("/")
-
-        decimal_latitude = utils.dms_coordinates_to_dd_coordinates(gps_latitude, gps_latitude_ref)
-        app.logger.debug(f"Latitude (DD): {decimal_latitude}")
-        decimal_longitude = utils.dms_coordinates_to_dd_coordinates(gps_longitude, gps_longitude_ref)
-        app.logger.debug(f"Longitude (DD): {decimal_longitude}")
-        coordinates = (decimal_latitude, decimal_longitude)
-        location_info = rg.search(coordinates, verbose=False)[0]
-        location_info['country'] = pycountry.countries.get(alpha_2=location_info['cc'])
-        answer = f"{location_info['name']}, {location_info['country'].name}"
-        app.logger.info(f"You were in {answer}!")
-        return render_template("locate.html", answer=answer,
-                               lat=decimal_latitude, lon=decimal_longitude)
-    elif request.form["action"] == "details":
-        image_members = utils.filter_out_tags(image)
-        camera_model = utils.format_camera_model(image)
-        return render_template("details.html", array=image_members, model=camera_model, image=image)
-    else:
-        flash('Requested action unrecognized', 'error')
-        app.logger.error(f"Requested action {request.form['action']} is unrecognized and "
-                         f"couldn't be processed.")
+    if not (gps_latitude or gps_latitude_ref or gps_longitude or gps_longitude_ref):
+        flash('No geolocation data found', 'error')
+        app.logger.warning("No geolocation data found")
         return redirect("/")
+
+    decimal_latitude = utils.dms_coordinates_to_dd_coordinates(gps_latitude, gps_latitude_ref)
+    app.logger.debug(f"Latitude (DD): {decimal_latitude}")
+    decimal_longitude = utils.dms_coordinates_to_dd_coordinates(gps_longitude, gps_longitude_ref)
+    app.logger.debug(f"Longitude (DD): {decimal_longitude}")
+    coordinates = (decimal_latitude, decimal_longitude)
+    location_info = rg.search(coordinates, verbose=False)[0]
+    location_info['country'] = pycountry.countries.get(alpha_2=location_info['cc'])
+    answer = f"{location_info['name']}, {location_info['country'].name}"
+    app.logger.info(f"You were in {answer}!")
+    return render_template("locate.html", answer=answer,
+                           lat=decimal_latitude, lon=decimal_longitude)
+
+
+@app.route("/details", methods=["POST"])
+def details():
+    file = request.files["image"]
+    image = Image(file.stream.read())
+
+    if not utils.check_if_exif(image):
+        flash('No EXIF data found', 'error')
+        app.logger.warning("No EXIF data found")
+        return redirect("/")
+
+    image_members = utils.filter_out_tags(image)
+    camera_model = utils.format_camera_model(image)
+    return render_template("details.html", array=image_members, model=camera_model, image=image)
 
 
 if __name__ == '__main__':
