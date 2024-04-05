beafb8f731fee30b58fcd3da64ee56842c559e51
Fabien Rica <fabien.rica@gmail.com>
2024-04-05 18:02:46 +0200

    clean utils.py: simplify methods based on nice redditor's code review, add comments and return values everywhere

--- utils.py
+++ utils.py
@@ -2,30 +2,17 @@ import logging
 import webbrowser
 
 
-def format_dms_coordinates(coordinates):
-    return f"{coordinates[0]}°{coordinates[1]}\'{coordinates[2]}\""
-
-
-def dms_coordinates_to_dd_coordinates(coordinates, coordinates_ref):
-    decimal_degrees = coordinates[0] + \
-                      coordinates[1] / 60 + \
-                      coordinates[2] / 3600
-
+def dms_coordinates_to_dd_coordinates(coordinates, coordinates_ref) -> float:
+    """ Convert coordinates from Degrees Minutes Seconds (DMS) to Decimal Degrees (DD)
+    (Most geolocation tools use DD) """
+    decimal_degrees = coordinates[0] + coordinates[1] / 60 + coordinates[2] / 3600
     if coordinates_ref == "S" or coordinates_ref == "W":
         decimal_degrees = -decimal_degrees
-
     return decimal_degrees
 
 
-# Not used anymore, I keep it for cli app
-def draw_map_for_location(latitude, latitude_ref, longitude, longitude_ref):
-    url = f"https://www.google.com/maps?q={latitude}{latitude_ref} {longitude}{longitude_ref}"
-    # print(f"url is {url}")
-    webbrowser.open_new_tab(url)
-
-
 def check_if_exif(image) -> bool:
-    """ Check if image has exif tags """
+    """ Check if image has exif tags and log it """
     ret = True
     if image.has_exif:
         status = f"contains EXIF (version {image.get('exif_version')}) information."
@@ -36,8 +23,33 @@ def check_if_exif(image) -> bool:
     return ret
 
 
-# Not used anymore, I keep it for cli app
-def display_details(image):
+def format_camera_model(image) -> str:
+    """ Capitalize camera model for cleaner output """
+    return image.model.capitalize()
+
+
+def filter_out_tags(image) -> list:
+    """ Remove tags with bytes values (like _interoperability_ifd_Pointer) """
+    ignore_prefixes = ['_', 'get', 'delete', 'list_all']
+    return [tag for tag in dir(image) if not any(tag.startswith(prefix) for prefix in ignore_prefixes)]
+
+
+# Below are methods I used in the beginning for a CLI version of the app
+
+
+def format_dms_coordinates(coordinates) -> str:
+    """ Format the DSM coordinates with the proper symbols """
+    return f"{coordinates[0]}°{coordinates[1]}\'{coordinates[2]}\""
+
+
+def draw_map_for_location(latitude, latitude_ref, longitude, longitude_ref) -> None:
+    """ Open a browser window showing location with Google Maps """
+    url = f"https://www.google.com/maps?q={latitude}{latitude_ref} {longitude}{longitude_ref}"
+    webbrowser.open_new_tab(url)
+
+
+def display_basic_picture_information(image):
+    """ Output basic exif information """
     image_members = [dir(image)]
     image_member_list = enumerate(image_members)
     nb_exif_tags = image_member_list.__sizeof__()
@@ -51,21 +63,3 @@ def display_details(image):
     print("-------------------------")
     print(f"{image.datetime_original}.{image.subsec_time_original} {image.get('offset_time', '')}\n")
     return image_member_list
-
-
-def format_camera_model(image) -> str:
-    # just to get a cleaner output in case the camera model is not capitalized
-    camera_model = image.model.capitalize()
-    return camera_model
-
-
-def filter_out_tags(image):
-    image_members = dir(image)
-    # remove from the list tags with bytes values like _interoperability_ifd_Pointer
-    image_members[:] = (tags for tags in image_members if
-                        (not tags.startswith("_")) and
-                        (not tags.startswith("get")) and
-                        (not tags.startswith("delete")) and
-                        (not tags.startswith("list_all")))
-    # app.logger.debug(image_members)
-    return image_members
